2f953e62de096967dfe24ea9cf99db41a122f763
smelicha <smelicha@student.42heilbronn.de>
2023-06-01 23:50:54 +0200

    updating some excersising test functions in test folder

--- test/test.c
+++ test/test.c
@@ -1,15 +1,27 @@
-#include <stdio.h>
 #include "testheader.h"
 
 int main(void)
 {
 	int	n;
+	t_data	*data;
 
 	n = 0;
+	data = malloc(sizeof(t_data));
+	data->first_link = new_list(n);
+	data->last_link = data->first_link;
 	while (n < 10)
 	{
+		data->last_link = add_link(data->last_link, n);
 		printf("return of the function: %d\n", fcn());
 		n++;
 	}
+	data->link = data->first_link;
+	while(data->link->next != NULL)
+	{
+		printf("From list: %d\n", data->link->fd);
+		data->link = data->link->next;
+	}
+	printf("\nbefore deleting list\n");
+	delete_list(data->first_link);
 	return (0);
 }
--- test/test1.c
+++ test/test1.c
@@ -1,7 +1,50 @@
+#include "testheader.h"
+
 int	fcn(void)
 {
 	static int	i;
 	
 	i++;
 	return (i);
+}
+
+t_fdlist *new_list(int value)
+{
+	t_fdlist	*new_list;
+
+	new_list = malloc(sizeof(t_fdlist));
+	new_list->fd = value;
+	new_list->next = NULL;
+	return (new_list);
+}
+
+t_fdlist *add_link(t_fdlist *current, int value)
+{
+	t_fdlist	*new_link;
+
+	new_link = malloc(sizeof(t_fdlist));
+	new_link->fd = value;
+	new_link->next = NULL;
+	current->next = new_link;
+	return (new_link);
+}
+void delete_list(t_fdlist *first_link)
+{
+	t_fdlist	*next;
+	t_fdlist	*current;
+
+	current = first_link;
+	next = first_link->next;
+	free(first_link);
+	next = current->next;
+	while (current->next != NULL)
+	{
+		next = current->next;
+		if (current != NULL)
+		{
+			printf("pointer to free: %p\n", current);
+			free(current);
+		}
+		current = next;
+	}
 }
